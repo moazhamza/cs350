da5edb8c16c8a6ca4cdffe6b1e0f6a9fadc30d0e
Moaz <moazhamza@gmail.com>
2017-09-11 15:42:38 -0400

    Began implementing no argument commands

--- Assignments/Assignment01/minish.c
+++ Assignments/Assignment01/minish.c
@@ -1,6 +1,56 @@
+//
+//  minish.c
+//  CS 350 Assignment01
+//
+//  Created by Moaz Hamza on 9/11/17.
+//  Copyright © 2017 Moaz Hamza. All rights reserved.
+//
+
+//printf, scanf
+#include <stdio.h>
+// fork
+#include <unistd.h>
+// exit
+#include <stdlib.h>
 
 
+#define MAX_LEN 1023
+#define FORK_FAILED -1
+#define IN_CHILD 0
 
-int main(){
+int main(int argc, const char * argv[]) {
+    
+    // Create a variable to recieve the input command.
+    char command_input[MAX_LEN];
+    
+    while(1){
+    
+        // Ask for the input
+        printf ("minish>");
+        scanf ("%s", command_input);
+    
+        // Fork and save the return value in pid
+        pid_t pid = fork();
+    
+        // Error handle the fork
+        if (pid == FORK_FAILED) {
+            fprintf(stderr, "fork failed\n");
+            exit(1);
+        } 
 
+        // Run the command in the child
+        
+        if (pid == IN_CHILD){
+            // exec command
+            
+            // Exit successfully
+            exit(0);
+        }
+        
+        // Wait for child to be finished in parent. pid is the child's proccess ID
+        if (pid > 0){
+            
+        }
+    }
+    return 0;
 }
